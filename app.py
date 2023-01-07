180341e81fc385652d13c259e6cdac6085e3918a
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-07 15:58:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,16 @@
 #Must Subscribe On YouTube @LazyDeveloperr 
 
 from flask import Flask
+from application import db
+from application.models import Person
 app = Flask(__name__)
+app.debug=True
 
 @app.route('/')
 def hello_world():
     return '@LazyDeveloper'
+manager= flast.ext.resless.APIManager(app,flask_sqlalchemy_db=db)
+manager.create_api(Person, methods=['GET','POST','DELETE'])
 
 
 if __name__ == "__main__":
